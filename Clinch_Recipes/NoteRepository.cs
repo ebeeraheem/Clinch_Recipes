c847b5d94c4298b8ec8e818df5583ba0e473018d
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-02 21:41:03 +0100

    Add note repository

---
+++ Clinch_Recipes/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using Clinch_Recipes.Data;
+using Clinch_Recipes.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinch_Recipes;
+
+public class NoteRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public NoteRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Note>> GetAllNotesAsync()
+    {
+        return await _context.Notes.ToListAsync();
+    }
+
+    public async Task<Note?> GetNoteByIdAsync(int id)
+    {
+        return await _context.Notes.FindAsync(id);
+    }
+
+    public async Task AddNoteAsync(Note note)
+    {
+        await _context.Notes.AddAsync(note);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateNoteAsync(Note note)
+    {
+        _context.Notes.Update(note);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> DeleteNoteAsync(int id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if (note is not null)
+        {
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        return false;
+    }
+}
