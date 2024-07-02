ed63b498296c6ff7f608b9b41d1d73dfbcb80f3a
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-02 21:22:29 +0100

    Add application db context

---
+++ Clinch_Recipes/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clinch_Recipes.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+    {
+    }
+}
