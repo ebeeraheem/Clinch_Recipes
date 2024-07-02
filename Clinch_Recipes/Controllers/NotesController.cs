ef34e420e5c60d8530cc7e0f7ed760a5c6443b4d
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-02 21:48:35 +0100

    Add notes controller

---
+++ Clinch_Recipes/Controllers/NotesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinch_Recipes.Controllers;
+public class NotesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
