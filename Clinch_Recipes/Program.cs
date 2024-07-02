7af5cf55d27ae935f71b5bf91aba9e7ddad0f3bb
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-02 22:14:28 +0100

    Register note repository in DI container

--- Clinch_Recipes/Program.cs
+++ Clinch_Recipes/Program.cs
@@ -1,4 +1,5 @@
 using Clinch_Recipes.Data;
+using Clinch_Recipes.Entities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
 
 var app = builder.Build();
 
