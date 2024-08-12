1663094a94440698d961fb2ec511974aff7008e0
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-08-12 18:10:04 +0100

    feat: remove redundant endpoints in note controller

--- Clinch_Recipes/Controllers/NotesController.cs
+++ Clinch_Recipes/Controllers/NotesController.cs
@@ -16,47 +16,7 @@ public class NotesController : Controller
         var notes = await _noteRepository.GetAllNotesAsync();
         return View(notes);
     }
-
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Create(Note note)
-    {
-        if (ModelState.IsValid)
-        {
-            note.CreatedDate = DateTime.UtcNow;
-            note.LastUpdatedDate = DateTime.UtcNow;
-            note.Content = Markdown.ToHtml(note.Content);
-
-            await _noteRepository.AddNoteAsync(note);
-            return RedirectToAction(nameof(Index));
-        }
-        return View(note);
-    }
-
-    public async Task<IActionResult> Edit(Guid id)
-    {
-        var note = await _noteRepository.GetNoteByIdAsync(id);
-        if (note is null) return NotFound();
-
-        return View(note);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Edit(Note note)
-    {
-        if (ModelState.IsValid)
-        {
-            note.LastUpdatedDate = DateTime.Now;
-            await _noteRepository.UpdateNoteAsync(note);
-            return RedirectToAction(nameof(Index));
-        }
-        return View(note);
-    }
-
+    
     public async Task<IActionResult> Upsert(Guid? id)
     {
         var note = new Note();
