df58656ef3eab9aa4942ba97f33395ba5c993c69
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-12-10 17:24:39 +0100

    add user entity

---
+++ Clinch_Recipes/UserEntity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Clinch_Recipes.UserEntity;
+
+public class ApplicationUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
