5825eb67f605a3dbd96069540f3b03a4998be958
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-12-17 13:30:10 +0100

    enable retry on failure for transient database errors

--- Clinch_Recipes/Program.cs
+++ Clinch_Recipes/Program.cs
@@ -11,7 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    options =>
+    {
+        options.EnableRetryOnFailure(3);
+    }));
 
 var issuer = builder.Configuration.GetValue<string>("Jwt:Issuer");
 var audience = builder.Configuration.GetValue<string>("Jwt:Audience");
